5011863fe4db1822273208787c19ff7c38242760
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-14 14:15:34 -0300

    feat: cria o router de user e adiciona o middleware de erro

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 const errorHandlerMiddleware = require('../middlewares/errorHandlerMiddleware');
 
-router.post('/', userController.login, errorHandlerMiddleware);
+router.post('/', userController.add, errorHandlerMiddleware);
 
 module.exports = router;
