c71a4bdaaf188fc61ee47aaa1c5843c801db5ca8
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-14 16:33:20 -0300

    refactor: altera o modo de exibição dos erros

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -2,11 +2,14 @@ const errors = {
   ValidationError: 400,
   NotFoundError: 400,
   UserAlreadyExistsError: 409,
+  TokenNotFoundError: 401,
+  UserNotFoundError: 400,
+  InvalidTokenError: 401,
 };
 
 const errorHandlerMiddleware = async ({ name, message }, _req, res, _next) => {
   const status = errors[name];
-  if (!status) return res.sendStatus(500);
+  if (!status) return res.status(500).json({ message });
   return res.status(status).json({ message });
 };
 
