262a44655fb2bc0688361bc6daafeaaa9ffb71ae
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-14 18:23:15 -0300

    feat: cria o modelo postCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,30 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", 
+  {
+    postId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true }
+  },
+  {
+    timestamps: false,
+    tableName: 'PostCategories'
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
