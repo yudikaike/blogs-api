79b09dc64a1c6f1bc844f12310c1a7d7a0bbe05a
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-14 14:11:02 -0300

    feat: cria validateBodyAdd e checkIfUserExists para validação e add para adicionar um usuário

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,5 +1,9 @@
 const Joi = require('joi');
-const { runSchema, throwNotFoundError } = require('./_services');
+const { 
+  runSchema, 
+  throwNotFoundError, 
+  throwUserAlreadyExistsError,
+} = require('./_services');
 const { User } = require('../database/models');
 
 const userServices = {
@@ -11,10 +15,26 @@ const userServices = {
     'any.required': 'Some required fields are missing',
   })),
 
+  validateBodyAdd: runSchema(Joi.object({
+    displayName: Joi.string().min(8).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    image: Joi.string(),
+  })),
+
   async checkIfLoginExists({ email, password }) {
     const user = await User.findOne({ where: { email, password } });
     if (!user) throwNotFoundError('Invalid fields');
-  }, 
+  },
+  
+  async checkIfUserExists({ email }) {
+    const user = await User.findOne({ where: { email } });
+    if (user) throwUserAlreadyExistsError('User already registered');
+  },
+
+  async add({ displayName, email, password, image }) {
+    await User.create({ displayName, email, password, image });
+  },
 };
 
 module.exports = userServices;
