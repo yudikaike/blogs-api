9c0395c0bf4079a3a1ed92ab47a9be3a801e3b3a
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-14 16:34:00 -0300

    feat: adiciona a rota para listar todos os usuários

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/userController');
 const errorHandlerMiddleware = require('../middlewares/errorHandlerMiddleware');
 
 router.post('/', userController.add, errorHandlerMiddleware);
+router.get('/', userController.list, errorHandlerMiddleware);
 
 module.exports = router;
