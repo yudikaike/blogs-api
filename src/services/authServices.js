f30446bfeb15665c88853c1ac1bb57ff747f8949
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-14 16:31:17 -0300

    feat: cria validateToken para validação e corrige criação do token

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,15 +1,32 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+const { 
+  throwTokenNotFoundError,
+  throwUserNotFoundError, 
+  throwInvalidTokenError } = require('./_services');
 
 const authServices = {
   async createToken(user) {
     const { password: _, ...restOfUser } = user;
-    const token = jwt.sign({ data: restOfUser.dataValues }, process.env.JWT_SECRET, { 
-      expiresIn: '5m',
+    const token = jwt.sign({ data: restOfUser }, process.env.JWT_SECRET, { 
+      expiresIn: '15m',
       algorithm: 'HS256',
      });
     
      return token;
+  },
+
+  async validateToken(token) {
+    try {
+      if (!token) return throwTokenNotFoundError('Token not found');
+      const { data: { email } } = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({ where: { email } });
+      if (!user) return throwUserNotFoundError('User not found');
+    } catch (err) {
+      if (err.name === 'JsonWebTokenError') throwInvalidTokenError('Expired or invalid token');
+      throw err;
+    }
   },
 };
 
