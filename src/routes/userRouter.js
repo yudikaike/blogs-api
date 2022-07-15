e677999a188bc3ed247873e6c779d6ed9e6bc511
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-15 11:43:01 -0300

    feat: cria a rota para apagar o usuário

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,5 +5,6 @@ const errorHandlerMiddleware = require('../middlewares/errorHandlerMiddleware');
 router.post('/', userController.add, errorHandlerMiddleware);
 router.get('/', userController.list, errorHandlerMiddleware);
 router.get('/:id', userController.get, errorHandlerMiddleware);
+router.delete('/me', userController.delete, errorHandlerMiddleware);
 
 module.exports = router;
