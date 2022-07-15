1875b737af6eabbe3b4c03f31d6ec00b9b0ff423
Kaike Yudi <kaikeyudi@outlook.com>
2022-07-15 12:17:34 -0300

    feat: cria rota para pesquisar post por title ou content

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const postController = require('../controllers/postController');
 const errorHandlerMiddleware = require('../middlewares/errorHandlerMiddleware');
 
+router.get('/search', postController.search, errorHandlerMiddleware);
 router.post('/', postController.add, errorHandlerMiddleware);
 router.get('/', postController.list, errorHandlerMiddleware);
 router.get('/:id', postController.get, errorHandlerMiddleware);
